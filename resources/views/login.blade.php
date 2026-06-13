84675c5623301dab642598a15baa6ae9152f5c07
tzuyu10 <faeldonia.10282005@gmail.com>
2026-06-14 03:32:10 +0800

    style: add logo

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ asset('build/assets/images/pup-logo.png') }}">
     <title>PUP Enrollment Portal</title>
     @vite([
         'resources/sass/app.scss',
